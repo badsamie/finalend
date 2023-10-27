f3fa843d4aebdde6ae8764bdd6b43d61372091a0
Sabina <sabina@MacBooks-MacBook-Air.local>
2023-10-27 15:16:17 +0600

    commit of chat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { ChatEngine } from "react-chat-engine";
+
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <ChatEngine
+        projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+        userName="사비나"
+        userSecret="12345"
+      />
+      <ChatEngine
+        projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+        userName="aisuluu"
+        userSecret="aisuluu"
+      />
+      <ChatEngine
+        projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+        userName="cha yan"
+        userSecret="123"
+      />
+      <ChatEngine
+        projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+        userName="보라"
+        userSecret="321"
+      />
+      <ChatEngine
+        projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+        userName="찬열"
+        userSecret="4321"
+      />
+
+    </>
+  );
 };
 
 export default App;
