53d7b6e0e04ea420106c80adbc1fdcb06015b7c0
Sabina <sabina@MacBooks-MacBook-Air.local>
2023-10-28 15:44:53 +0600

    commit of quiz

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { ChatEngine } from "react-chat-engine";
+
+
+const Chat = () => {
+  return (
+    <>
+    <ChatEngine
+      projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+      userName="사비나"
+      userSecret="12345"
+    />
+    <ChatEngine
+      projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+      userName="aisuluu"
+      userSecret="aisuluu"
+    />
+    <ChatEngine
+      projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+      userName="cha yan"
+      userSecret="123"
+    />
+    <ChatEngine
+      projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+      userName="보라"
+      userSecret="321"
+    />
+    <ChatEngine
+      projectID="ec22ee5d-dd02-4053-9558-d7faa7502c73"
+      userName="찬열"
+      userSecret="4321"
+    />
+  </>
+  )
+}
+
+export default Chat
--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -46,9 +46,56 @@ const questions = [
     ],
     correct: 2,
   },
+  {
+    title: "We ... to work every day.",
+    variants: ["gos", "goes", "go", "are going"],
+    correct: 2,
+  },
+  {
+    title: "Nick is Russian. He’s ... Russia",
+    variants: ["from", "to", "on", "at"],
+    correct: 0,
+  },
+  {
+    title: "Moscow is ... than London.",
+    variants: ["nosiest", "noisy", "noisier", "most noisy"],
+    correct: 2,
+  },
+  {
+    title: "Look at Emma! She ... a blue skirt today!",
+    variants: ["wearing", "wear", "is wear", "is wearing"],
+    correct: 2,
+  },
+  {
+    title: "She’ll have to wait because the breakfast ... .",
+    variants: [
+      "is just making",
+      "is just being made",
+      "is just being making",
+      "is just made",
+    ],
+    correct: 1,
+  },
 ];
 
 function Result({ correct }) {
+  const percentageCorrect = (correct / questions.length) * 100;
+
+  let level;
+  if (percentageCorrect > 85) {
+    level = "Advanced";
+  } else if (percentageCorrect > 70) {
+    level = "Upper-Intermediate";
+  } else if (percentageCorrect > 60) {
+    level = "Intermediate";
+  } else if (percentageCorrect > 40) {
+    level = "Pre-Intermediate";
+  } else if (percentageCorrect > 20) {
+    level = "Elementary";
+  } else {
+    level = "Beginner";
+  }
+
   return (
     <div className="result text-center">
       <img
@@ -56,35 +103,57 @@ function Result({ correct }) {
         className="w-32 h-32 mx-auto mb-6 "
         alt="Result Icon"
       />
-      <h2 className="text-2xl font-bold mb-6">
-        Вы отгадали {correct} ответов из {questions.length}
+      <h2 className="text-2xl font-bold uppercase mb-6 hover:text-3xl hover:text-violet-500">
+        Вы отгадали {correct} ответов из {questions.length}.
       </h2>
-      <a href="/">
-        <button className="bg-blue-500 hover:bg-blue-600 text-white rounded px-6 py-2 shadow transition-all duration-200">
-          Попробовать снова
+      <h3 className="text-xl font-bold uppercase mb-6 hover:text-2xl hover:text-violet-600">
+        {" "}
+        Ваш уровень: {level}
+      </h3>
+      <a href="/quiz">
+        <button className="bg-violet-500 hover:bg-violet-600 font-light uppercase text-white rounded px-6 py-2 shadow transition-all duration-200">
+          Пойти улучшать свой уровень
         </button>
       </a>
     </div>
   );
 }
 
-function Game({ step, question, onClickVariant }) {
-  const percentage = Math.round((step / questions.length) * 100);
+function ProgressBar({ percentage }) {
   return (
-    <div className="flex flex-col">
-      <div className=" progress w-full bg-gray-300 rounded-full h-4 mb-6">
-        <div
-          style={{ width: `${percentage}%` }}
-          className="progress__inner bg-blue-400 transition-all duration-300 rounded-full"
-        ></div>
+    <div className="w-full bg-gray-200 rounded-full mb-4 overflow-hidden">
+      <div
+        style={{ width: `${percentage}%` }}
+        className="h-4 bg-violet-400 transition-all duration-500"
+      ></div>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariant, lives, message }) {
+  const increment = 100 / questions.length;
+  const percentage = increment * (step + 1);
+
+  return (
+    <div className="flex flex-col items-center space-y-4 w-9/12">
+      <ProgressBar percentage={percentage} />
+      <div className="flex space-x-2 mb-4">
+        {Array.from({ length: 5 }).map((_, i) => (
+          <div
+            key={i}
+            className={`w-4 h-4 ${
+              i < lives ? "bg-violet-400" : "bg-gray-300"
+            } rounded-full`}
+          ></div>
+        ))}
       </div>
-      <h1 className="text-2xl font-bold mb-6">{question.title}</h1>
-      <ul className="space-y-4">
+      <h1 className="text-2xl font-bold mb-6 text-center">{question.title}</h1>
+      <ul className="w-full space-y-4">
         {question.variants.map((text, index) => (
           <li
             onClick={() => onClickVariant(index)}
             key={text}
-            className="cursor-pointer p-4 bg-white rounded shadow hover:bg-gray-100 transition-all duration-200"
+            className="cursor-pointer p-4 bg-white w-full text-center rounded-lg shadow hover:bg-gray-100 transition-all duration-200"
           >
             {text}
           </li>
@@ -97,25 +166,101 @@ function Game({ step, question, onClickVariant }) {
 const Quiz = () => {
   const [step, setStep] = useState(0);
   const [correct, setCorrect] = useState(0);
+  const [lives, setLives] = useState(5);
+  const [message, setMessage] = useState(null);
+  const [isCorrect, setIsCorrect] = useState(true);
 
   const question = questions[step];
 
   const onClickVariant = (index) => {
-    setStep(step + 1);
     if (index === question.correct) {
+      setIsCorrect(true);
+      setMessage("ПРАВИЛЬНО!");
       setCorrect(correct + 1);
+    } else {
+      setIsCorrect(false);
+      setMessage(
+        "НЕВЕРНО! ПРАВИЛЬНЫЙ ОТВЕТ: " + question.variants[question.correct]
+      );
+      setLives(lives - 1);
+    }
+  
+    setTimeout(() => {
+      setMessage(null);
+      setStep(prevStep => prevStep + 1);
+    }, 1000);
+  };
+
+  const handleNavigation = (action) => {
+    switch (action) {
+      case "prev":
+        if (step > 0) {
+          setStep(step - 1);
+        }
+        break;
+      case "next":
+        if (step < questions.length - 1) {
+          setStep(step + 1);
+        }
+        break;
+      case "skip":
+        if (step < questions.length - 1) {
+          setStep(step + 1);
+        }
+        break;
+      default:
+        break;
     }
   };
 
   return (
     <div className="App min-h-screen bg-blue-50 flex items-center justify-center p-4">
-      {step !== questions.length ? (
-        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      {lives > 0 && step !== questions.length ? (
+        <div className="flex flex-col items-center space-y-4 w-9/12">
+          <Game
+            step={step}
+            question={question}
+            onClickVariant={onClickVariant}
+            lives={lives}
+            message={message}
+          />
+          <div className="mt-4">
+            <button
+              onClick={() => handleNavigation("prev")}
+              className="border uppercase w-36 bg-violet-400 rounded text-white h-9"
+            >
+              Prev
+            </button>
+            <button
+              onClick={() => handleNavigation("skip")}
+              className="border uppercase w-36 h-9 bg-violet-400 rounded text-white"
+            >
+              Skip
+            </button>
+            <button
+              onClick={() => handleNavigation("next")}
+              className="border uppercase w-36 h-9 bg-violet-400 rounded text-white"
+            >
+              Next
+            </button>
+          </div>
+        </div>
       ) : (
         <Result correct={correct} />
       )}
+
+      <div
+        className={`absolute bottom-0 left-0 right-0 p-4  text-white text-center transform transition-transform duration-300 ${
+          message
+            ? isCorrect
+              ? "bg-green-300 translate-y-0"
+              : "bg-red-300 translate-y-0"
+            : "translate-y-full"
+        }`}
+      >
+        {message}
+      </div>
     </div>
   );
 };
-
 export default Quiz;
