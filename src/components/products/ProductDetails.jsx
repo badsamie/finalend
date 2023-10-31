17516cb5c1baf797323f23744fd5cfe1f1aee7d0
badsamie <collection826@gmail.com>
2023-10-31 14:39:25 +0600

    852

--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -2,16 +2,11 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import {
-  addRating,
   deleteProduct,
   getOneProduct,
-  getProducts,
 } from "../../store/products/productsActions";
 import { clearOneProductState } from "../../store/products/productsSlice";
 import { removeFromCart, toggleCart } from "../../store/cart/cartSlice";
-import { createOrder } from "../../store/cart/cartSlice"; // Import createOrder
-import { isUserLogin } from "../../helpers/functions";
-
 import ProductsRating from "./ProductsRating";
 import {
   removeAllFromFav,
@@ -25,15 +20,10 @@ import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import LoadingPage from "../../pages/LoadingPage";
-
 const ProductDetails = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
-  const { loading, oneProduct, rating } = useSelector(
-    (state) => state.products
-  );
-
+  const { loading, oneProduct } = useSelector((state) => state.products);
   const { id } = useParams();
   const cartItems = useSelector((state) => state.cart.items) || [];
   const favItems = useSelector((state) => state.fav.items) || [];
@@ -44,7 +34,7 @@ const ProductDetails = () => {
   }, [dispatch, id]);
 
   if (!oneProduct) {
-    return <LoadingPage />
+    return <LoadingPage />;
   }
 
   const isItemInCart = cartItems.some((item) => item.id === oneProduct.id);
@@ -69,7 +59,6 @@ const ProductDetails = () => {
   const handleDelete = () => {
     dispatch(deleteProduct({ id: oneProduct.id }));
     dispatch(removeFromCart(oneProduct.id));
-    dispatch(removeAllFromFav(oneProduct.id));
     navigate("/products");
   };
 
@@ -77,15 +66,11 @@ const ProductDetails = () => {
     <>
       {loading ? (
         <LoadingPage />
-       
       ) : (
         <>
           {oneProduct && (
             <div className="product-details-container">
               <h3 className="product-title">{oneProduct.title}</h3>
-      ) : (
-        <>
-        
               {oneProduct.images.length > 0 && (
                 <img
                   className="product-image"
@@ -97,7 +82,6 @@ const ProductDetails = () => {
                 <p>Location: {oneProduct.location}</p>
                 <p>Price: {oneProduct.price}</p>
                 <p className="price-in-dollars">$: {oneProduct.price_dollar}</p>
-              
                 <p>Education: {oneProduct.education}</p>
               </div>
               <div className="product-description">
@@ -108,22 +92,6 @@ const ProductDetails = () => {
                 <p>Rating: {oneProduct ? oneProduct.rating : "No rating"}</p>
                 <p>Likes: {oneProduct.like_count}</p>
                 <div className="comments-section">
-                <p>{oneProduct.updated_at}</p>
-                <p>Rating: {oneProduct.rating}</p>
-              </div>
-
-              <div className="mt-4">
-                <button
-                  onClick={() => {
-                    dispatch(addRating({}));
-                    dispatch(getProducts());
-                  }}
-                >
-                  Send
-                </button>
-                <p>Rating: {oneProduct ? oneProduct.rating : "Нет рейтинга"}</p>
-                <p>like:{oneProduct.like_count}</p>
-                <div>
                   {oneProduct.comments.map((comment) => (
                     <div key={comment.id} className="comment">
                       <span className="comment-owner">@{comment.owner}</span>
@@ -143,14 +111,13 @@ const ProductDetails = () => {
                 </button>
                 <button className="cart-button" onClick={handleCartAction}>
                   {isItemInCart ? (
-                    <ShoppingCartIcon />
-                  ) : (
                     <RemoveShoppingCartIcon />
+                  ) : (
+                    <ShoppingCartIcon />
                   )}
                 </button>
                 <button className="fav-button" onClick={handleFavAction}>
                   {!isItemInFav ? <BookmarkBorderIcon /> : <BookmarkIcon />}
-
                 </button>
                 <ProductsRating />
                 <ProductLike />
